f943578e8acfd1376097c6fbfbb6e03de0147557
Tom Kerkhove <kerkhove.tom@gmail.com>
2023-07-29 21:30:54 +0200

    Scaffold
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/WebAssembly.Runtime/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAssembly.Runtime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
